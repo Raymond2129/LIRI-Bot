ecdc01d2a6e6bf87b4df517475e21dd3d252c3a6
Ray Ray <raymond2129@gmail.com>
2019-02-04 18:18:41 -0700

    final update

--- liri.js
+++ liri.js
@@ -11,7 +11,7 @@ var parameter = process.argv[3];
 
 
 
-
+// create a switchCase and enter the 4 needed cases. Concert, spotify, movie, and do what it says
 function switchCase() {
 
   switch (action) {
@@ -21,11 +21,11 @@ function switchCase() {
       break;                          
 
     case 'spotify-this-song':
-      spotSong(parameter);
+      spotifySong(parameter);
       break;
 
     case 'movie-this':
-      movieInfo(parameter);
+      movieThis(parameter);
       break;
 
     case 'do-what-it-says':
@@ -33,35 +33,40 @@ function switchCase() {
       break;
 
       default:                            
-      logIt("Invalid Instruction");
+      logData("Invalid Instruction");
       break;
 
   }
 };
-
+//function for concert this. 
 function bandsInTown(parameter){
-
+//if concert this take the process.argv at array position 3 and make it the new varaible. 
 if (action === 'concert-this')
 {
-	var movieName="";
+	var artistName="";
 	for (var i = 3; i < process.argv.length; i++)
 	{
-		movieName+=process.argv[i];
+		artistName+=process.argv[i];
 	}
-	console.log(movieName);
+	console.log(artistName);
 }
 else
 {
-	movieName = parameter;
+	artistName = parameter;
 }
 
 
+//list the url and add the artist name into the url
+var queryUrl = "https://rest.bandsintown.com/artists/"+artistName+"/events?app_id=codecademy";
 
-var queryUrl = "https://rest.bandsintown.com/artists/"+movieName+"/events?app_id=codecademy";
+//console.log(queryUrl)
 
+//need to send the url request and recieve error, response,and body info back
 
 request(queryUrl, function(error, response, body) {
 
+
+//if error is false and the response code is 200 (meaning it recieved data) the parse the JSON file and log the data to screen along with appending the log file
   if (!error && response.statusCode === 200) {
 
     var JS = JSON.parse(body);
@@ -72,27 +77,30 @@ request(queryUrl, function(error, response, body) {
         var year = dTime.substring(0,4);
         var day = dTime.substring(8,10);
         var dateForm = month + "/" + day + "/" + year
-  
-      logIt("\n---------------------------------------------------\n");
+        //function logData to append to the log.txt file
+      logData("\n---------------------------------------------------\n");
 
         
-      logIt("Date: " + dateForm);
-      logIt("Name: " + JS[i].venue.name);
-      logIt("City: " + JS[i].venue.city);
+      logData("Date: " + dateForm);
+      logData("Name: " + JS[i].venue.name);
+      logData("City: " + JS[i].venue.city);
+      //need to clean up data, if the region is empty do not list and if it is list the state.
       if (JS[i].venue.region !== "")
       {
-        logIt("Country: " + JS[i].venue.region);
+        logData("Country: " + JS[i].venue.region);
       }
-      logIt("Country: " + JS[i].venue.country);
-      logIt("\n---------------------------------------------------\n");
+      logData("Country: " + JS[i].venue.country);
+      logData("\n---------------------------------------------------\n");
 
     }
   }
 });
 }
-function spotSong(parameter) {
 
+//function to run the spotify song search
+function spotifySong(parameter) {
 
+//need an if else if nothing is entered it will search for The Sign by ace of base else run a search on the enterd song
   var searchTrack;
   if (parameter === undefined) {
     searchTrack = "The Sign ace of base";
@@ -100,58 +108,69 @@ function spotSong(parameter) {
     searchTrack = parameter;
   }
 
+  //use spotify.search with the type as track
   spotify.search({
     type: 'track',
+    //query will be the var holding the entered parameters
     query: searchTrack
   }, function(error, data) {
     if (error) {
-      logIt('Error occurred: ' + error);
+      logData('Error occurred: ' + error);
       return;
     } else {
-      logIt("\n---------------------------------------------------\n");
-      logIt("Artist: " + data.tracks.items[0].artists[0].name);
-      logIt("Song: " + data.tracks.items[0].name);
-      logIt("Preview: " + data.tracks.items[3].preview_url);
-      logIt("Album: " + data.tracks.items[0].album.name);
-      logIt("\n---------------------------------------------------\n");
+
+        //log the data to the terminal and to the log.txt
+      logData("\n---------------------------------------------------\n");
+      logData("Artist: " + data.tracks.items[0].artists[0].name);
+      logData("Song: " + data.tracks.items[0].name);
+      logData("Preview: " + data.tracks.items[3].preview_url);
+      logData("Album: " + data.tracks.items[0].album.name);
+      logData("\n---------------------------------------------------\n");
       
     }
   });
 };
-function movieInfo(parameter) {
 
+//need to create a function to run the movie-this 
+function movieThis(parameter) {
 
+//if else statement if no movie entered it will search for Mr. Nobody else take user parameters and search for the movie
   var findMovie;
   if (parameter === undefined) {
     findMovie = "Mr. Nobody";
   } else {
     findMovie = parameter;
   };
-
+//list the queryURL and enter the movie varaible use the trilogy key
   var queryUrl = "http://www.omdbapi.com/?t=" + findMovie + "&y=&plot=short&apikey=trilogy";
   
+  //run a requst that takes the url and runs a function to get the errors resposne and JSON body file
   request(queryUrl, function(err, res, body) {
-  	var bodyOf = JSON.parse(body);
+      var bodyOf = JSON.parse(body);
+
+      //if error is false and the response code is 200 (meaning it recieved data) the parse the JSON file and log the data to screen along with appending the log file
     if (!err && res.statusCode === 200) {
-      logIt("\n---------------------------------------------------\n");
-      logIt("Title: " + bodyOf.Title);
-      logIt("Release Year: " + bodyOf.Year);
-      logIt("IMDB Rating: " + bodyOf.imdbRating);
-      logIt("Rotten Tomatoes Rating: " + bodyOf.Ratings[1].Value); 
-      logIt("Country: " + bodyOf.Country);
-      logIt("Language: " + bodyOf.Language);
-      logIt("Plot: " + bodyOf.Plot);
-      logIt("Actors: " + bodyOf.Actors);
-      logIt("\n---------------------------------------------------\n");
+
+        //log the data to the terminal along with appending the log.txt
+      logData("\n---------------------------------------------------\n");
+      logData("Title: " + bodyOf.Title);
+      logData("Release Year: " + bodyOf.Year);
+      logData("IMDB Rating: " + bodyOf.imdbRating);
+      logData("Rotten Tomatoes Rating: " + bodyOf.Ratings[1].Value); 
+      logData("Country: " + bodyOf.Country);
+      logData("Language: " + bodyOf.Language);
+      logData("Plot: " + bodyOf.Plot);
+      logData("Actors: " + bodyOf.Actors);
+      logData("\n---------------------------------------------------\n");
     }
   });
 };
-
+//create a function to run the do what it says
 function getRandom() {
 fs.readFile('random.txt', "utf8", function(error, data){
 
     if (error) {
-        return logIt(error);
+        return logData(error);
       }
 
   
@@ -160,7 +179,7 @@ fs.readFile('random.txt', "utf8", function(error, data){
     if (dataArr[0] === "spotify-this-song") 
     {
       var songcheck = dataArr[1].trim().slice(1, -1);
-      spotSong(songcheck);
+      spotifySong(songcheck);
     } 
     else if (dataArr[0] === "concert-this") 
     { 
@@ -182,20 +201,20 @@ fs.readFile('random.txt', "utf8", function(error, data){
     else if(dataArr[0] === "movie-this") 
     {
       var movie_name = dataArr[1].trim().slice(1, -1);
-      movieInfo(movie_name);
+      movieThis(movie_name);
     } 
     
     });
 
 };
-
-function logIt(dataToLog) {
+//create a function to console log to the terminal along with appending the log.txt file. Need to create a way to log errors in case it does not work properly
+function logData(dataToLog) {
 
 	console.log(dataToLog);
 
 	fs.appendFile('log.txt', dataToLog + '\n', function(err) {
 		
-		if (err) return logIt('Error logging data to file: ' + err);	
+		if (err) return logData('Error logging data to file: ' + err);	
 	});
 }
 
